731552f7395425d5de9122f4bceaae31e600ebbf
varnan <varnant@gmail.com>
2022-05-29 20:31:02 +0530

    mul() added by varnan

--- One.java
+++ One.java
@@ -2,6 +2,9 @@ class One{
 void sub(int a,int b){
 System.out.println(a-b);
 }
+void mul(int a){
+System.out.println(a*a);
+}
 public static void main(string[]){
 System.out.println("hello git");
 }
